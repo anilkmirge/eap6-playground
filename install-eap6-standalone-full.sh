9b4ca273f024c79f7e072f8290781079277f1d78
Anil Mirge <amirge@vizuri.com>
2013-12-31 21:34:15 -0500

    Adding the script files for standalone mode for default and full profile and for domain mode

---
+++ install-eap6-standalone-full.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+#
+# JBoss EAP Accelerator script - Owned and Reserved by Vizuri
+#
+sudo
+echo  '##############################################################################################################################################'
+echo  '##############################################################################################################################################'
+echo  '#################################### Welcome to JBoss EAP Installation and Configuration set up (Standalone mode using Full profile) ################################'
+echo  '##############################################################################################################################################'
+echo  '##############################################################################################################################################'
+eap_path=$1
+dir_path=$2
+
+if [[ -z "$dir_path" || -z "$eap_path" ]]
+then echo Please enter a valid path of JBoss EAP archive amd  where you would like to install JBoss EAP as the first and second argument, and then try again. For e.g. sudo ./install-eap-standalone.sh /home/username/Downloads /opt/server
+exit 0
+else
+echo You have chosen to install the JBoss EAP in $dir_path directory.
+
+if [ -d $dir_path ]
+then cp $eap_path/jboss-eap-6.*.*.zip $dir_path
+  if [ -f $eap_path/jboss-eap-6.*.*.zip ]
+  then echo Successfully copied the eap archive into $dir_path
+  else echo There was some problem copying the JBoss EAP archive into $dir_path. Please check the permissions.
+  exit 0
+  fi
+else echo Did not find the directory $dir_path, so creating one.. 
+mkdir -p "$dir_path"
+  if [ -d $dir_path ]
+  then echo Successfully created the directory $dir_path
+  else echo There was some problem creating the dir $dir_path. Please check the permissions.
+  exit 0
+  fi
+cp $eap_path/jboss-eap-6.*.*.zip $dir_path
+  if [ -f $eap_path/jboss-eap-6.*.*.zip ]
+  then echo Successfully copied the eap archive into $dir_path
+  else echo There was some problem copying the JBoss EAP archive into $dir_path. Please check the permissions.
+  exit 0
+  fi
+fi
+echo About to unzip the jboss-eap-6.x.x.zip file into $dir_path
+cd $dir_path
+jar xvf jboss-eap-6.*.*.zip
+echo '######################## Successfully extracted the JBoss EAP archive ########################'
+
+cd $dir_path/jboss-eap-6.*/bin
+echo Setting permission to all the script files ....
+chmod 755 *.sh
+echo Done...permission set.
+echo Now starting the jboss eap in standalone mode using full profile.....
+./standalone.sh --server-config=standalone-full.xml
+fi
+#End of the script
